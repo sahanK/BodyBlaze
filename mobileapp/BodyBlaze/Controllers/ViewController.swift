918d5dc8f68486b445d4abcff61e824ada183b71
Sahan Kaushalya <sahankaushalya01@gmail.com>
2023-04-17 23:32:36 +0530

    Added initial iOS project

---
+++ mobileapp/BodyBlaze/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BodyBlaze
+//
+//  Created by Sahan Walpita on 2023-04-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
